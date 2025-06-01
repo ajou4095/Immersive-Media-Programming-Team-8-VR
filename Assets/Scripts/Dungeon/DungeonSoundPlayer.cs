d90a975c3ed1decbdfb4fc7f31d3534f5e643ba0
Ray Jang <ajou4095@gmail.com>
2025-06-01 17:54:59 +0900

    Add DungeonSoundPlayer

---
+++ Assets/Scripts/Dungeon/DungeonSoundPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DungeonSoundPlayer : MonoBehaviour
+{
+    public AudioSource unlockDoorSuccessSound;
+    public AudioSource unlockDoorFailureSound;
+    
+    void Start()
+    {
+        var audioArray = GetComponents<AudioSource>();
+        unlockDoorSuccessSound = audioArray[0];
+        unlockDoorFailureSound = audioArray[1];
+    }
+}
